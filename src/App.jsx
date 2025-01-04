f4e71f67d143168f1bfd39a874fb0a4a56e8422d
belen002 <belenberhanu15@gmail.com>
2025-01-04 22:23:30 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./components/Header"
+import Alert from "./components/Alert"
+import First from "./components/FirstSection"
+import Second from "./components/SecondSection"
+import Third from "./components/ThirdSection"
+import Fourth from "./components/FourthSection"
+import Fifth from "./components/FifthSection"
+import Sixth from "./components/SixthSection"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Alert/>
+    <First/>
+    <Second/>
+    <Third/>
+    <Fourth/>
+    <Fifth/>
+    <Sixth/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
